21fc986fc3e8493d342e5a0066aba5a739431dfd
Rashi Bajpai <rashibajpai1999@gmail.com>
2024-09-08 21:41:48 +0530

    Update Prediction Pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,5 +1,7 @@
 import os 
 import sys
+import pandas as pd
+import numpy as np
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from dataclasses import dataclass
 from src.logger import logging
@@ -19,7 +21,7 @@ from src.components.data_transformation import DataTransformation
 
 # file path to save the best model
 class ModelTrainingConfig:
-    trained_model_file_path = os.path.join("artifacts","model_training.pkl")
+    trained_model_file_path = os.path.join("artifacts","model.pkl")
 
 class ModelTrainer:
     def __init__(self):
@@ -60,26 +62,28 @@ class ModelTrainer:
                 },
                 "Gradient Boosting":{
                      'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
-                    'learning_rate':[.1,.01,.05,.001],
+                #    'learning_rate':[.1,.01,.05,.001],
                     'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
-                     'criterion':['squared_error', 'friedman_mse'],
-                     'max_features':['auto','sqrt','log2'],
-                    'n_estimators': [8,16,32,64,128,256]
+                #     'criterion':['squared_error', 'friedman_mse'],
+                #     'max_features':['auto','sqrt','log2'],
+                 #   'n_estimators': [8,16,32,64,128,256]
                 },
                 "Linear Regression":{},
+                "XGBRFRegressor":{},
+                "KNN":{},
                 "XGBRegressor":{
-                    'learning_rate':[.1,.01,.05,.001],
+                #    'learning_rate':[.1,.01,.05,.001],
                     'n_estimators': [8,16,32,64,128,256]
                 },
                 "CatBoosting Regressor":{
-                    'depth': [6,8,10],
+                #    'depth': [6,8,10],
                     'learning_rate': [0.01, 0.05, 0.1],
-                    'iterations': [30, 50, 100]
+                #    'iterations': [30, 50, 100]
                 },
                 "AdaBoost Regressor":{
                     'learning_rate':[.1,.01,0.5,.001],
-                    'loss':['linear','square','exponential'],
-                    'n_estimators': [8,16,32,64,128,256]
+                #    'loss':['linear','square','exponential'],
+                #    'n_estimators': [8,16,32,64,128,256]
                 }
                 
             }
